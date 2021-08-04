4f929a658b09f5846418f48347d08ee42ceff4fc
vamsivammu <vammuvamsi64@gmail.com>
2021-08-05 01:17:06 +0530

    small changes

--- src/subscription-view/models/subscription-view.entity.ts
+++ src/subscription-view/models/subscription-view.entity.ts
@@ -4,7 +4,7 @@ import { ViewColumn, ViewEntity } from "typeorm";
 @ViewEntity({expression:`
     select "subscription"."startTimestamp" as "startTimestamp","subscription"."endTimestamp" as "endTimestamp", "subscription"."userId" as "userId",
     "payment"."subscription" as "planId" from "subscription" left join "payment" on "payment"."sessionId" = "subscription"."paymentId"
-`, materialized:true,synchronize:true,name:'subscription_view'})
+`,synchronize:true,name:'subscription_view'})
 export class SubscriptionView{
     @ViewColumn()
     startTimestamp:Date;
